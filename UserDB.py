eede5743f2327c134885b64bb4d42012dc9baf9c
smitbose <bosmit90@gmail.com>
2016-12-22 15:32:51 +0530

    changed SQLlit to MySQL

--- UserDB.py
+++ UserDB.py
@@ -1,34 +1,63 @@
-import sqlite3
+import MySQLdb
 import json
 import time
 import following
 import os
+import tweepy
+import logging
+import datetime
+
 USER_DIR = 'twitter-followings'
 
-def minefriends(api,centre):
 
-    print("The user currently being added/checked",centre)
-    conn = sqlite3.connect('doge.sql')
-    conn.execute('''CREATE TABLE IF NOT EXISTS Politicians(UserName text,Location text,FollowerCount int,PRIMARY KEY
-    (UserName))''')
+def minefriends(api, centre):
+    while True:
+        try:
+            print("The user currently being added/checked", centre)
+            conn = MySQLdb.connect("localhost", "root", "1234", "twitter_graph")
+            cursor = conn.cursor()
 
-    userfname = os.path.join(USER_DIR,str(centre)+'.json')
-    if not os.path.exists(userfname):
-        user = api.get_user(centre)
-        time.sleep(60)
-        conn.execute('INSERT OR IGNORE INTO Politicians VALUES(?,?,?)', (user.screen_name, user.location, user.followers_count))
-        conn.commit()
-        conn.close()
+            userfname = os.path.join(USER_DIR, str(centre) + '.json')
+            if not os.path.exists(userfname):
+                user = api.get_user(centre)
+                time.sleep(60)
+                sql_stmt = 'INSERT OR IGNORE INTO Politicians VALUES(%s,%s,%d)' % (
+                    str(user.screen_name), str(user.location), user.followers_count)
+                try:
+                    cursor.execute(sql_stmt)
+                    conn.commit()
+                except:
+                    conn.rollback()
 
-        following_names = following.get_following(api, centre)
+                conn.close()
 
-        d = {'name': user.name,
-             'screen_name': user.screen_name,
-             'id': user.id,
-             'friends_count': user.friends_count,
-             'followers_count': user.followers_count,
-             'following_names': following_names}
+                following_names = following.get_following(api, centre)
 
-        with open(userfname, 'w') as outf:
-            outf.write(json.dumps(d, indent=1))
-            outf.close()
+                d = {'name': user.name,
+                     'screen_name': user.screen_name,
+                     'id': user.id,
+                     'friends_count': user.friends_count,
+                     'followers_count': user.followers_count,
+                     'following_names': following_names}
+
+                with open(userfname, 'w') as outf:
+                    outf.write(json.dumps(d, indent=1))
+                    outf.close()
+                break
+        except tweepy.TweepError as error:
+            print(str(error))
+
+            if str(error) == 'Not authorized.':
+                print('Can''t access user data - not authorized.')
+                break
+            if str(error) == 'User has been suspended.':
+                print('User suspended.')
+                break
+            if str(error) == "[{'message': 'Rate limit exceeded', 'code': 88}]" or str(error) == "[{u'message': " \
+                                                                                                 "u'Rate limit " \
+                                                                                                 "exceeded', " \
+                                                                                                 "u'code': 88}]":
+                print('Rate limited. Sleeping for 15 minutes.')
+                logging.debug('Rate limit exceeded at %s' % str(datetime.now()))
+                time.sleep(15 * 60 + 15)
+        continue
--- following.py
+++ following.py
@@ -4,15 +4,14 @@ from datetime import datetime
 import logging
 
 
-def get_following(api,centre):
-
+def get_following(api, centre):
     following = []
     while True:
         try:
             i = 1
-            for friends in tweepy.Cursor(api.friends,screen_name=centre,count=200).items():
+            for friends in tweepy.Cursor(api.friends, screen_name=centre, count=200).items():
                 # print i,friends.screen_name
-                i = i+1
+                i += 1
                 following.append(friends.screen_name)
                 if i % 200 == 0:
                     time.sleep(60)
@@ -26,7 +25,8 @@ def get_following(api,centre):
             if str(error) == 'User has been suspended.':
                 print('User suspended.')
                 break
-            if str(error) == "[{'message': 'Rate limit exceeded', 'code': 88}]" or str(error) == "[{u'message': u'Rate limit exceeded', u'code': 88}]":
+            if str(error) == "[{'message': 'Rate limit exceeded', 'code': 88}]" or str(
+                    error) == "[{u'message': u'Rate limit exceeded', u'code': 88}]":
                 print('Rate limited. Sleeping for 15 minutes.')
                 logging.debug('Rate limit exceeded at %s' % str(datetime.now()))
                 time.sleep(15 * 60 + 15)
